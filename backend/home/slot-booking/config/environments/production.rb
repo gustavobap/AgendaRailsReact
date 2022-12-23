a1298f203fe1b1954dddb8db8cda3c219bbd4b97
Gustavo Peixoto <gustavo.bul@gmail.com>
2022-12-23 02:34:20 +0100

    configure allowed origin for action cable

--- backend/home/slot-booking/config/environments/production.rb
+++ backend/home/slot-booking/config/environments/production.rb
@@ -37,7 +37,8 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
-
+  # config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
+  config.action_cable.allowed_request_origins = ["http://#{ENV['CLIENT_IP']}", "https://#{ENV['CLIENT_IP']}"]
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
