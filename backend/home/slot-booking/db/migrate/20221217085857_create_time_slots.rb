781fa09500921d093de4c498c677f3cb06259696
Gustavo Peixoto <gustavo.bul@gmail.com>
2024-12-03 21:07:38 +0100

    add backend as a gitmodule

--- backend/home/slot-booking/db/migrate/20221217085857_create_time_slots.rb
+++
@@ -1,7 +0,0 @@
-class CreateTimeSlots < ActiveRecord::Migration[7.0]
-  def change
-    create_table :time_slots do |t|
-      t.tsrange :duration, null: false, index: true
-    end
-  end
-end
--- backend/home/slot-booking/db/migrate/20221219034742_create_booking_day_locks.rb
+++
@@ -1,7 +0,0 @@
-class CreateBookingDayLocks < ActiveRecord::Migration[7.0]
-  def change
-    create_table :booking_day_locks do |t|
-      t.timestamp :day, null: false, unique: true
-    end
-  end
-end
